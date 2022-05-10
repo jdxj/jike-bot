bf1c00db21ac163ea46120313faf262fdb1fc078
wangyq <985759262@qq.com>
2022-05-10 10:48:59 +0800

    refactor: Makefile

--- scripts/make-rules/k8s.mk
+++ scripts/make-rules/k8s.mk
@@ -1,18 +1,28 @@
+KUBE_CONFIG := --kubeconfig=$(DEPLOY)/kube_config.yaml
+
+.PHONY: k8s.create.ns
+k8s.create.ns:
+	@kubectl $(KUBE_CONFIG) create namespace jike
+
 .PHONY: k8s.create.secret
 k8s.create.secret:
-	@kubectl -n jike scale deployment jike-bot-dep --replicas=0
-	@kubectl -n jike delete secrets jike-bot-config
-	@kubectl -n jike create secret generic jike-bot-config --from-file=$(DEPLOY)/config.yaml
-	@kubectl -n jike scale deployment jike-bot-dep --replicas=1
+	@kubectl $(KUBE_CONFIG) -n jike create secret generic jike-bot-config --from-file=$(DEPLOY)/config.yaml
+
+.PHONY: k8s.apply.secret
+k8s.apply.secret:
+	@kubectl $(KUBE_CONFIG) -n jike scale deployment jike-bot-dep --replicas=0
+	@kubectl $(KUBE_CONFIG) -n jike delete secrets jike-bot-config
+	@kubectl $(KUBE_CONFIG) -n jike create secret generic jike-bot-config --from-file=$(DEPLOY)/config.yaml
+	@kubectl $(KUBE_CONFIG) -n jike scale deployment jike-bot-dep --replicas=1
 
 .PHONY: k8s.scale.%
 k8s.scale.%:
-	@kubectl -n jike scale deployment jike-bot-dep --replicas=$*
+	@kubectl $(KUBE_CONFIG) -n jike scale deployment jike-bot-dep --replicas=$*
 
 .PHONY: k8s.create.deploy
 k8s.create.deploy:
-	@kubectl -n jike create -f deploy/deploy.yaml
+	@kubectl $(KUBE_CONFIG) -n jike create -f $(DEPLOY)/deployment.yaml
 
 .PHONY: k8s.set.image
 k8s.set.image:
-	@kubectl -n jike set image deployment jike-bot-dep jike-bot-c=$(DOCKER_TAG)
+	@kubectl $(KUBE_CONFIG) -n jike set image deployment jike-bot-dep jike-bot-c=$(DOCKER_TAG)
